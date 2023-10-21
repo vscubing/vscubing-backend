1c9aa88905d4cddf4a6a419c5ac946b373153149
homa <antonsav123@gmail.com>
2023-10-22 00:09:46 +0200

    static was added to urls

--- vscubing/urls.py
+++ vscubing/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/swagger/', include('apps.swagger.urls')),
     path('api/accounts/', include('apps.accounts.urls')),
     path('api/contests/', include('apps.contests.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
