b457a39c8e038d1c88a60fee1e31b5457d78de00
homa <antonsav123@gmail.com>
2023-10-21 21:12:02 +0200

    GOOGLE_REDiRECT_URL changed to 3000 port

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -8,7 +8,7 @@ from django.views.generic import RedirectView
 
 from scripts.fake_data import create_contest
 
-GOOGLE_REDIRECT_URL = 'http://127.0.0.1'
+GOOGLE_REDIRECT_URL = 'http://127.0.0.1:3000'
 
 
 class GoogleLoginView(SocialLoginView):
