e05218952c65ed70d4036607f8c55cc71fe3ce4c
Anton Savytskyi <antonsav123@gmail.com>
2023-10-24 17:26:33 +0200

    solve reconstrunction endpoint added

--- apps/contests/urls.py
+++ apps/contests/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('contest/<int:contest_number>/<str:discipline>/', ContestView.as_view(), name='past-contest'),
     path('solve_contest/<int:contest_number>/<str:discipline>/', SolveContestView.as_view(), name='solve-contest'),
     path('ongoing_contest_number/', OngoingContestNumberView.as_view(), name='solve-contest'),
-    path('solve_info/<int:id>/', SolveReconstructionSerializer.as_view(), name='solve-info'),
+    path('solve_reconstruction/<int:id>/', SolveReconstructionSerializer.as_view(), name='solve-info'),
 ]
