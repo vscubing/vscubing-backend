cbfeb8caf2e0601dca4561fa642ca7a6cae6898e
HomaDev <antonsav123@gmail.com>
2024-02-02 14:59:41 +0100

    add some more endpoints mapping

--- apps/contests/apis.py
+++ apps/contests/apis.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView, Response
 from drf_spectacular.views import extend_schema
+from rest_framework import serializers
 
 
 @extend_schema(
@@ -16,9 +17,46 @@ class SolveListApi(APIView):
 @extend_schema(
     responses={200: {'json': 'data'}}
 )
+class SolveRetrieveApi(APIView):
+    def get(self, request, pk):
+        # selectors: select one solve by id
+        # filters: filter with filter serializer
+        # serializers: serialize
+        return Response(data={'json': 'data'})
+
+
+class SolveListBestInDisciplineList(APIView):
+    def get(self, request):
+        return Response(data={'json', 'data'})
+
+
+class SolveListBestOfEveryUser(APIView):
+    def get(self, request):
+        return Response(data={'json', 'data'})
+
+
+class SolveCreateApi(APIView):
+    pass
+
+
+class SolveSubmitApi(APIView):
+    pass
+
+
+@extend_schema(
+    responses={200: {'json': 'data'}}
+)
 class RoundSessionWithSolvesListApi(APIView):
     def get(self, request):
         # selectors: select needed round_session models with nested solves
         # filters
         # serializers
         return Response(data={'json': 'data'})
+
+
+class ContestListApi(APIView):
+    def get(self, request):
+        return Response(data={'json': 'data'})
+
+
+
