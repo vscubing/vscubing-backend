dfcc2c358a6cf69b9b1721f464b0389e81d82695
HomaDev <antonsav123@gmail.com>
2024-05-29 21:10:19 +0200

    uncomment router

--- apps/contests/urls.py
+++ apps/contests/urls.py
@@ -23,7 +23,7 @@ solves_urlpatterns = [
     path('single-result-leaderboard', SingleResultLeaderboardApi.as_view(), name='list')
     # path('create/', SolveCreateApi.as_view(), name='create'),
     # path('submit/<int:pk>/', SolveSubmitApi.as_view(), name='submit'),
-    # path('best-in-every-discipline/', SolveListBestInEveryDiscipline.as_view(), name='list-best-in-every-discipline'),
+    path('best-in-every-discipline/', SolveListBestInEveryDiscipline.as_view(), name='list-best-in-every-discipline'),
     # path('best-of-every-user/', SolveListBestOfEveryUser.as_view(), name='list-best-of-every-user'),
 ]
 
