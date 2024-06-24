0f47795d86e8da08935ad7c18330a3c2600e7ce8
Anton Savytskyi <antonsav123@gmail.com>
2024-06-24 12:54:19 +0200

    update ContestListApi

--- apps/contests/selectors.py
+++ apps/contests/selectors.py
@@ -224,7 +224,7 @@ class ContestSelector:
     def list(self, filters=None):
         filters = filters or {}
 
-        contest_set = ContestModel.objects.all()
+        contest_set = ContestModel.objects.filter(is_ongoing=False)
         return ContestFilter(filters, contest_set).qs
 
 
