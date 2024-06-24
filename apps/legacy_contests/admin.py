19cd7247943113d6253dc289dca88ad14609a25c
Anton Savytskyi <antonsav123@gmail.com>
2024-06-24 12:19:48 +0200

    remove legacy-contests and docs directories

--- apps/legacy_contests/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import DisciplineModel, ScrambleModel, ContestModel, SolveModel, RoundSessionModel
-
-admin.site.register([DisciplineModel, ScrambleModel, ContestModel, SolveModel, RoundSessionModel])
