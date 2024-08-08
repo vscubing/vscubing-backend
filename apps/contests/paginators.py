62199bf8152265ddabea920a8b17564068a3406e
Anton Savytskyi <antonsav123@gmail.com>
2024-08-08 19:56:37 +0200

    fix pagination no solves bug

--- apps/contests/paginators.py
+++ apps/contests/paginators.py
@@ -89,7 +89,10 @@ def page_size_page_paginator(queryset, page_size, page, extra=0):
     total_items = queryset.count()
     total_pages = math.ceil(total_items / page_size)
     if total_pages < page:
-        raise NotFoundException('Invalid page.')
+        if page != 1:
+            raise NotFoundException('Invalid page.')
+        elif page == 1:
+            pass
 
     start = (page - 1) * page_size
     end = page * page_size + extra
