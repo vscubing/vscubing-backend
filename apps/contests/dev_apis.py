365a53015134ea8dfc4725d0bcfe159f8d8d2a66
Anton Savytskyi <antonsav123@gmail.com>
2024-08-09 16:33:49 +0200

    update OwnRoundSessionApi schema

--- apps/contests/dev_apis.py
+++ apps/contests/dev_apis.py
@@ -2,14 +2,34 @@ from rest_framework.response import Response
 from rest_framework.views import APIView, status
 from rest_framework.permissions import IsAuthenticated
 from django.core.exceptions import ObjectDoesNotExist
+from drf_spectacular.utils import extend_schema, OpenApiResponse, OpenApiExample
+from drf_spectacular.types import OpenApiTypes
 
-from .models import RoundSessionModel
 from .general_services import current_contest_retrieve
 
 
 class OwnRoundSessionDeleteApi(APIView):
     permission_classes = [IsAuthenticated]
 
+    @extend_schema(
+        responses={
+            204: None,  # No content
+            400: None,  # No content
+            401: OpenApiResponse(
+                examples=[
+                    OpenApiExample(
+                        'Unauthorized',
+                        value={
+                            'detail': 'Authentication credentials were not provided.'
+                        },
+                        response_only=True,
+                        status_codes=['401']
+                    )
+                ],
+                response=OpenApiTypes.OBJECT
+            )
+        },
+    )
     def delete(self, request):
         user_id = request.user.id
         contest = current_contest_retrieve()
